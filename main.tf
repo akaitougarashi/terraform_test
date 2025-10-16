71ef8760bf9c9397458c5c0be278cb12c50d43f1
Ken Aoi ISZJ <Ken_Aoi@isuzu.com>
2025-10-16 11:25:59 +0900

    firstcommit

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws"{
+    profile = "terraform"
+    region = "ap-northeast-1"
+}
+
+resource "aws_instance" "hello-world"{
+    ami = "ami-070e0d4707168fc07"
+    instance_type = "t2.micro"
+}
