80dae2b27d3dfdba3bfca647e681190864f8a084
Ken Aoi ISZJ <Ken_Aoi@isuzu.com>
2025-10-18 12:54:37 +0900

    test

---
+++ rds.tf
@@ -0,0 +1,16 @@
+# RDS parameter froup
+resource "aws_db_parameter_group" "mysql_standalone_parametergroup"{
+    name = "${var.project}-${var.environment}-mysql-standalone-parametergroup"
+    family = "mysql8.0"
+
+    parameter {
+      name = "charavter_set_database"
+      value = "utf8mb4"
+    }
+
+    parameter{
+        name = "character_set_server"
+        value = "utf8mb4"
+    }
+}
+
